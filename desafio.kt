93573f15f95962c99bc6b527a4d30f2244f7e883
CalvinDuque <126295518+CalvinDuque@users.noreply.github.com>
2024-01-11 19:30:13 -0300

    Comentando o codígo para melhor manutenção futura.

--- desafio.kt
+++ desafio.kt
@@ -1,7 +1,8 @@
+//Niveis dos cursos da DIO
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario(val nome: String)
-
+//Conteudo do curso da DIO
 data class Conteudo(var nome: String, var duração: Int = 0) {
     override fun toString(): String {
         return "Conteudo(nome='$nome', duração=$duração)"
@@ -26,19 +27,20 @@ override fun toString(): String {
 }
 
 fun main() {
+    // Criação de instâncias de Conteudo
     val kotlinBasico = Conteudo("Kotlin Básico", 60)
     val kotlinIntermediario = Conteudo("Kotlin Intermediário", 120)
     val kotlinAvancado = Conteudo("Kotlin Avançado", 180)
-
+	// Criação de uma instância de Formacao
     val formacaoKotlin = Formacao("Formação Kotlin", listOf(kotlinBasico, kotlinIntermediario, kotlinAvancado))
-
+	// Criação de instâncias de Usuario
     val usuario1 = Usuario("Maria Cristina")
     val usuario2 = Usuario("Calvin Duque")
     val usuario3 = Usuario("FalvoJr")
-
+	// Matrícula de usuários na formação
     formacaoKotlin.matricular(usuario1)
     formacaoKotlin.matricular(usuario2)
     formacaoKotlin.matricular(usuario3)
-
+	// Impressão dos detalhes da formação
     println("Detalhes da formação:\n$formacaoKotlin")
 }
