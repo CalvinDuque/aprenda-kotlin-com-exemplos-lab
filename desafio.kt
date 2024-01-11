f433c45064e96412fa434f5f1b4a12ba34df8e1e
CalvinDuque <126295518+CalvinDuque@users.noreply.github.com>
2024-01-11 19:19:45 -0300

    Ajuste no código, utilizando todas as classes disponíveis, e melhorando apresentação do mesmo.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
+data class Conteudo(var nome: String, var duração: Int = 0) {
+    override fun toString(): String {
+        return "Conteudo(nome='$nome', duração=$duração)"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, var conteudos: List<Conteudo>) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    val inscritos = mutableListOf<Usuario>() 
+   	
 
-    val inscritos = mutableListOf<Usuario>()
-    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("A matrícula de ${usuario.nome} foi realizada com sucesso, em $nome.\n")
+    }
+
+override fun toString(): String {
+     val conteudosString = conteudos.joinToString(separator = "\n") { it.toString() }
+     val inscritosString = inscritos.joinToString(", ") { it.nome }
+     return "'$nome'\nConteúdos:\n$conteudosString\nInscritos: $inscritosString\n"
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlinBasico = Conteudo("Kotlin Básico", 60)
+    val kotlinIntermediario = Conteudo("Kotlin Intermediário", 120)
+    val kotlinAvancado = Conteudo("Kotlin Avançado", 180)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(kotlinBasico, kotlinIntermediario, kotlinAvancado))
+
+    val usuario1 = Usuario("Maria Cristina")
+    val usuario2 = Usuario("Calvin Duque")
+    val usuario3 = Usuario("FalvoJr")
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoKotlin.matricular(usuario3)
+
+    println("Detalhes da formação:\n$formacaoKotlin")
 }
